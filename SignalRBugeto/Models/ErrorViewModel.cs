136ad169a9a763472dbb6383e9e3bb7c450f9138
Yousof <yousof.hosseinpour@gmail.com>
2024-05-16 23:35:01 +0330

    initial project

---
+++ SignalRBugeto/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SignalRBugeto.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
